b431a9260283be6543d65c87d4e9841ac1b879f5
alvaromago <aalvaromago@gmail.com>
2024-04-08 19:58:12 +0200

    added all structure & classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import CarList from "./pages/CarListing/CarList";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
 	return (
-		<div>
-			<p className="text-blue-800">Bienvenido a mi aplicación con React y Tailwind</p>
-		</div>
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/carlist" element={<CarList />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</Router>
 	);
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,33 @@
+import "./Home.css";
+//import { useState } from "react";
+
+/*const navigation = [
+	{ name: "Product", href: "#" },
+	{ name: "Features", href: "#" },
+	{ name: "Marketplace", href: "#" },
+	{ name: "Company", href: "#" },
+];*/
+
+function Home() {
+	return (
+		<div>
+			<div>
+				<header>
+					<nav>
+						<div>
+							<p>Primera cosa</p>
+						</div>
+						<div>
+							<p>Segunda cosa</p>
+						</div>
+						<div>
+							<p>Tercera cosa</p>
+						</div>
+					</nav>
+				</header>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
