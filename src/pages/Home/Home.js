ae156c7667c6cf20f2b8f7078e952db27ba7b107
alvaromago <aalvaromago@gmail.com>
2024-04-09 11:50:53 +0200

    started landing page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,22 +9,48 @@ import "./Home.css";
 
 function Home() {
 	return (
-		<div>
-			<div>
-				<header>
-					<nav>
+		<div className="bg-background">
+			{
+				//Nav
+			}
+			<div className="text-center">
+				<h1 className="text-text py-10 font-extrabold">Título de la página</h1>
+				<div className="bg-secondary mx-8">
+					<div className="py-3">
 						<div>
-							<p>Primera cosa</p>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								class="lucide lucide-search"
+							>
+								<circle cx="11" cy="11" r="8" />
+								<path d="m21 21-4.3-4.3" />
+							</svg>
 						</div>
-						<div>
-							<p>Segunda cosa</p>
-						</div>
-						<div>
-							<p>Tercera cosa</p>
-						</div>
-					</nav>
-				</header>
+						<div>Search</div>
+					</div>
+					<div className="py-3">
+						<button className="bg-primary text-text font-semibold  py-3 px-10 border rounded-xl">Mostrar resultados</button>
+					</div>
+				</div>
+				<div>
+					<ul>
+						<li>
+							<img src="" alt="" />
+						</li>
+					</ul>
+				</div>
 			</div>
+			{
+				// Footer
+			}
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		colors: {
+			text: "#ffffff",
+			background: "#000000",
+			primary: "#358238",
+			secondary: "#ffffff",
+			accent: "#5a17a1",
+		},
 		extend: {},
 	},
 	plugins: [],
