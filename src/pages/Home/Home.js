b1cce74483822cbbcc6100daeedc01bbb4e49750
alvaromago <aalvaromago@gmail.com>
2024-04-09 08:22:22 +0200

    small css changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,9 @@ import "./Home.css";
 //import { useState } from "react";
 
 /*const navigation = [
-	{ name: "Product", href: "#" },
-	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" },
+	{ name: "About", href: "#" },
+	{ name: "CarList", href: "#" },
+	{ name: "Contact", href: "#" },
 ];*/
 
 function Home() {
